d1b53959f12476aeaee79c9b1ef16e4ebacbd4a3
Friyaldhy Aldhy <friyaldhy@gmail.com>
2026-06-04 14:07:44 +0700

    belajar if elif else

---
+++ gaji.py
@@ -0,0 +1,8 @@
+gaji = int(input("masukkan gaji: "))
+
+if jika gaji >= 10.000.000:
+    print("high income")
+    elif gaji >= 5.000.000:
+        print("middle income")
+        else selain itu gaji < 5.000.000:
+            print("low income")
